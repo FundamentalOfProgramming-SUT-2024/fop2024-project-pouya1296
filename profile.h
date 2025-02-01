893e7e0c78ad0f51b397525f2cadd38e84ecbe10
pouya1296 <pouyaastarakimain@gmail.com>
2025-02-01 11:26:46 +0330

    Add files via upload

--- profile.h
+++ profile.h
@@ -1,6 +1,55 @@
 #include "ncurses.h"
 #include <string.h>
 
+typedef struct {
+    char username[100];
+    char password[100];
+    char email[100];
+    char question[100];
+    char answer[100];
+} usEr;
+
+int loAduser(char *username, usEr *addeduser) {
+    MYSQL *sql;
+    MYSQL_RES *resault;
+    MYSQL_ROW row;
+
+    char *server = "localhost";
+    char *user = "root";
+    char *password = "pouya1234";
+    char *database = "userdatabase";
+
+    sql = mysql_init(NULL);
+   
+  mysql_real_connect(sql, server, user, password, database, 0, NULL, 0);
+ 
+    char query[256];
+    snprintf(query, sizeof(query), "SELECT username, password, email, question, answer FROM users WHERE username='%s'", username);
+    if(mysql_query(sql, query)){
+
+    return 0;
+    }
+    
+    
+
+    resault = mysql_store_result(sql);
+    if(resault == NULL)
+    return 0;
+
+    row = mysql_fetch_row(resault);
+    if(row == NULL)
+    return 0;
+        strcpy(addeduser->username, row[0]);
+        strcpy(addeduser->password, row[1]);
+        strcpy(addeduser->email, row[2]);
+        strcpy(addeduser->question, row[3]);
+        strcpy(addeduser->answer, row[4]);
+    
+    mysql_free_result(resault);
+    mysql_close(sql);
+    return 1;
+}
+
 void printprof() {
     char text[][100] = {        
         " _____  _____   ____  ______ _____ _      ______ ",
@@ -23,36 +72,16 @@ void printprof() {
 
 
 void printprofile(char newusername[100], WINDOW *window){
-        char information[110] = {0};
-        int found =0;
-    FILE *user = fopen("userinformation.txt", "r");
-    int valid =0;
-    int printflag =2;
-    while (fgets(information, 100, user ))
-    {
-        int removelast = strlen(information) -1;
-        information[removelast] = '\0';
-        if(found){
-            if(printflag == 5){
-                break;
-            }
-            else{
-                mvwprintw(window,printflag,1, "%s\n", information);
-                wrefresh(window);
-                printflag++;
-            }
-        }
-        
-       if(strncmp("username:", information, 9 ) ==0){
-        if(strcmp(information + 9, newusername ) == 0){
-           found =1;
-           mvwprintw(window,1,1, "%s\n", information);
-        }
-       }
-    }
-    fclose(user);
-  
+     usEr asghar;
+     loAduser(newusername, &asghar);
+        mvwprintw(window, 1, 1, "username: %s", asghar.username);
+         mvwprintw(window, 2, 1, "email: %s", asghar.email);
+         mvwprintw(window, 3, 1, "question: %s", asghar.question);
+         mvwprintw(window, 4, 1, "answer: %s", asghar.answer);
+
+   
 }
+
 void startprofile(char username[100]){
     initscr();
     start_color();
@@ -62,7 +91,7 @@ void startprofile(char username[100]){
     
 
         int boxheight = 20;
-    int boxwidth = 70;
+    int boxwidth = 80;
     int y_box = 5 + (LINES - boxheight) /2;
     int x_box = (COLS - boxwidth) /2;
     init_pair(1,COLOR_GREEN,COLOR_BLACK);
@@ -70,7 +99,7 @@ void startprofile(char username[100]){
     printprof();
     attroff(COLOR_PAIR(1));
 
-   FILE *file = fopen("userinformation.txt", "a");
+  // FILE *file = fopen("userinformation.txt", "a");
    WINDOW *window = newwin(boxheight, boxwidth, y_box, x_box);
    box(window, 0, 0);
    wbkgd(window, COLOR_PAIR(1));
